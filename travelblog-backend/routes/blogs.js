fd041a2b544836613790df9790676175a822e9f9
Fabian Kuenzle <fabian.kuenzle@gmail.com>
2021-02-25 19:32:25 +0100

    add blog entry endpoint

--- travelblog-backend/routes/blogs.js
+++ travelblog-backend/routes/blogs.js
@@ -2,49 +2,78 @@ const express = require('express');
 const assert = require('assert').strict;
 const router = express.Router();
 const dbConnectionString = 'mongodb+srv://weblab:weblab@cluster0.8q4oo.mongodb.net/traveblog?retryWrites=true&w=majority';
-const MongoClient = require('mongodb').MongoClient;
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+const ObjectId = mongodb.ObjectId
 const bodyParser = require('body-parser');
 
 const jsonParser = bodyParser.json()
 
 /* GET alls blogs. */
 router.get('/', function(req, res, next) {
-    const client = new MongoClient(dbConnectionString, { useNewUrlParser: true, useUnifiedTopology: true });
-    client.connect(function(err) {
+    const mongoClient = new MongoClient(dbConnectionString, { useNewUrlParser: true, useUnifiedTopology: true });
+    mongoClient.connect(function(err) {
         if (err) {
             res.sendStatus(500);
             throw err;
         }
 
-        let blogs = client.db("travelblog").collection('blogs');
+        let blogs = mongoClient.db("travelblog").collection('blogs');
         blogs.find().toArray((err, docs) => {
-            assert.equal(null, err);
+            assert.equal(err, null);
             res.status(200).json(docs);
-            client.close();
+            mongoClient.close();
         });
     });
 });
 
 /* POST create blog. */
 router.post('/', jsonParser, function(req, res, next) {
-    const client = new MongoClient(dbConnectionString, { useNewUrlParser: true, useUnifiedTopology: true });
-    client.connect(function(err) {
-        if (err) {
-            res.sendStatus(500);
-            throw err;
-        }
-        const blog = req.body;
+    const blog = req.body;
 
-        if (blog["title"] && blog["destination"] && blog["traveltime"] && blog["shortDescription"]) {
-            let blogs = client.db("travelblog").collection('blogs');
+    if (blog["title"] && blog["destination"] && blog["traveltime"] && blog["shortDescription"]) {
+        const mongoClient = new MongoClient(dbConnectionString, { useNewUrlParser: true, useUnifiedTopology: true });
+        mongoClient.connect(function(err) {
+            if (err) {
+                res.sendStatus(500);
+                throw err;
+            }
+
+            let blogs = mongoClient.db('travelblog').collection('blogs');
             blogs.insertOne(blog, function(err, result) {
+                assert.equal(err, null);
                 assert.equal(blog, result.ops[0], 'Blog from request has to equal to inserted blog in db.');
                 console.log(`Inserted ${result} into blogs.`);
-                client.close();
+                mongoClient.close();
                 res.sendStatus(201);
             });
-        }
-    });
+        });
+    } else {
+        res.status(400).send("A blog has to consist of a title, destination, traveltime (in days) and shortDescription.");
+    }
+});
+
+router.post('/:id/entries', jsonParser, function(req, res, next) {
+    const entry = req.body;
+
+    if (entry["title"] && entry["datetime"] && entry["text"]) {
+        const mongoClient = new MongoClient(dbConnectionString, { useNewUrlParser: true, useUnifiedTopology: true });
+        mongoClient.connect(function(err) {
+            if (err) {
+                res.sendStatus(500);
+                throw err;
+            }
+
+            mongoClient.db('travelblog').collection('blogs').updateOne({ _id: ObjectId(req.params.id) }, { $push: { entries: entry } }, function(err, result) {
+                console.log(result);
+                assert.equal(result.modifiedCount, 1, "Exactly the one blog addressed with the id should be changed.");
+                mongoClient.close();
+                res.sendStatus(201);
+            });
+        });
+    } else {
+        res.status(400).send("A blog entry has to consist of a title, datetime and text.");
+    }
 });
 
 module.exports = router;
