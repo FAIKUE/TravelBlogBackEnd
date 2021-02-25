837dcebffd742544769c7a886b1863805271c795
Fabian Kuenzle <fabian.kuenzle@gmail.com>
2021-02-25 15:06:02 +0100

    fix assert in post blog; add get all blogs

--- travelblog-backend/routes/blogs.js
+++ travelblog-backend/routes/blogs.js
@@ -9,7 +9,20 @@ const jsonParser = bodyParser.json()
 
 /* GET alls blogs. */
 router.get('/', function(req, res, next) {
-  res.send("hello");
+    const client = new MongoClient(dbConnectionString, { useNewUrlParser: true, useUnifiedTopology: true });
+    client.connect(function(err) {
+        if (err) {
+            res.sendStatus(500);
+            throw err;
+        }
+
+        let blogs = client.db("travelblog").collection('blogs');
+        blogs.find().toArray((err, docs) => {
+            assert.equal(null, err);
+            res.status(200).json(docs);
+            client.close();
+        });
+    });
 });
 
 /* POST create blog. */
@@ -17,6 +30,7 @@ router.post('/', jsonParser, function(req, res, next) {
     const client = new MongoClient(dbConnectionString, { useNewUrlParser: true, useUnifiedTopology: true });
     client.connect(function(err) {
         if (err) {
+            res.sendStatus(500);
             throw err;
         }
         const blog = req.body;
@@ -24,9 +38,10 @@ router.post('/', jsonParser, function(req, res, next) {
         if (blog["title"] && blog["destination"] && blog["traveltime"] && blog["shortDescription"]) {
             let blogs = client.db("travelblog").collection('blogs');
             blogs.insertOne(blog, function(err, result) {
-                assert.equal(blog, result);
+                assert.equal(blog, result.ops[0], 'Blog from request has to equal to inserted blog in db.');
                 console.log(`Inserted ${result} into blogs.`);
                 client.close();
+                res.sendStatus(201);
             });
         }
     });
